cef1cea65ad647af77fe0d568ee2702019472f11
lLouu <lLouu@users.noreply.github.com>
2020-11-08 15:27:15 +0100

    Part 1
    translate_to_bin(FILE* target, FILE* output)  -  edit the output file to be the binary of the target file
    
    count(FILE*)  -  count the number of char in the file

--- functions/question.c
+++ functions/question.c
@@ -1,2 +1,28 @@
 #include "../headers/question.h"
 
+//1.A
+void translate_to_bin(FILE* file_target, FILE* file_output)
+{
+    char ch = 0;
+    while((ch = fgetc(file_target)) != EOF && ch != '\n')
+    {
+        for (int i = 7; i >= 0; i--) {
+            fprintf(file_output, ((ch >> i) & 1)?"1":"0");
+        }
+    }
+}
+
+//1.B
+int count(FILE* target)
+{
+    int i = 0;
+    char c = fgetc(target);
+
+    while(c != EOF)
+    {
+        i++;
+        c = fgetc(target);
+    }
+
+    return i;
+}
--- headers/prototype/question_proto.h
+++ headers/prototype/question_proto.h
@@ -4,4 +4,7 @@
 #include "../common/header.h"
 #include "../common/struct.h"
 
+void translate_to_bin(FILE* file_target, FILE* file_output);
+int count(FILE* target);
+
 #endif
