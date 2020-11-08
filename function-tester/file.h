ef347a36d6743dabf6921783d03adaf38eb4e1bd
lLouu <lLouu@users.noreply.github.com>
2020-11-08 15:06:25 +0100

    Header of constant for dedicated placment of target and output test files

---
+++ function-tester/file.h
@@ -0,0 +1,9 @@
+#ifndef H_FILE
+#define H_FILE
+
+#define TUTURU "C:/Users/guerr/Documents/GitHub/Huffman-Project/file/tuturu.txt"
+#define OTHER "C:/Users/guerr/Documents/GitHub/Huffman-Project/file/other-target.txt"
+#define TREE_TARG "C:/Users/guerr/Documents/GitHub/Huffman-Project/file/tree.txt"
+#define OUT "C:/Users/guerr/Documents/GitHub/Huffman-Project/file/out.txt"
+
+#endif
