c814e160d44294a93abe6910c3994877e38b8d55
lLouu <lLouu@users.noreply.github.com>
2020-11-16 13:53:58 +0100

    main function

--- headers/.main.h
+++ headers/.main.h
@@ -1,9 +1,13 @@
 #ifndef MAIN
 #define MAIN
 
+#include "common/header.h"
+
 #include "prototype/options_proto.h"
 #include "prototype/encrypt_proto.h"
 #include "prototype/decrypt_proto.h"
+
+#define OPTION_PATH "C:/Users/guerr/Documents/GitHub/Custom-Compression-System/option.txt"
 
 #define FILE_WEIGHT 4
 #define FILE_ID     1
--- main.c
+++ main.c
@@ -2,5 +2,23 @@
 
 int main()
 {
+    option* run = (option*)malloc(sizeof(option));
+    read_option_file(run, OPTION_PATH);
+    char* tree_path = get_tree_path(run);
+    char* compress_path = get_compress_path(run);
+
+    if(run->mode)
+    {
+        encrypt(run->source, tree_path, compress_path);
+    }
+    else
+    {
+        decrypt(compress_path, tree_path, run->out);
+    }
+    
+
+    free(tree_path);
+    free(compress_path);
+    free_option(run);
     return 0;
 }
